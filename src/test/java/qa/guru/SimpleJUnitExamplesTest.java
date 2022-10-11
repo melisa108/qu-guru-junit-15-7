e4299b0c9ce4572d21814fef7cbcd81d834f42c2
melisa108 <kagyulife@gmail.com>
2022-10-11 09:31:50 +0200

    added simple test

---
+++ src/test/java/qa/guru/SimpleJUnitExamplesTest.java
@@ -0,0 +1,14 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleJUnitExamplesTest {
+    @Disabled
+    @DisplayName("this test")
+    @Test
+    void simpleTest0(){
+
+    }
+}
